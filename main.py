7142e2901128bfd1dfdc53170bda3c8dafca5cb0
Cauã Franco <caua.augusto3210@gmail.com>
2025-07-23 14:20:36 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,152 @@
+import sqlite3
+import csv
+
+
+class Database:
+    def __init__(self, db_file):
+        self.db_file = db_file
+        self.con = None
+        self.cursor = None
+
+    def connect(self):
+        self.con = sqlite3.connect(self.db_file)
+        self.cursor = self.con.cursor()
+
+    def execute(self, sql):
+        if not self.cursor:
+            raise RuntimeError("Database was not connected")
+        self.cursor.execute(sql)
+
+    def executemany(self, sql, params):
+        if not self.cursor:
+            raise RuntimeError("Database was not connected")
+        self.cursor.executemany(sql, params)
+
+    def commit(self):
+        if self.con:
+            self.con.commit()
+
+
+    def close(self):
+        if self.con:
+            self.con.close()
+
+# função para converter os tipos do python para tipos sql
+def sqlite_types(val):
+    if isinstance(val, int):
+        return "INTEGER"
+    elif isinstance(val, float):
+        return "REAL"
+    else:
+        return "TEXT"
+
+# funçao para retornar o tipo de variavel que esta no dicionario(valores)
+def get_types(val):
+    try:
+        return int(val)
+    except ValueError:
+        try:
+            return float(val)
+        except ValueError:
+            return val
+
+# funçao para retornar o nome do arquivo EX: alunos.csv -> alunos
+def get_table_name(csv_path):
+    return csv_path.split("/")[-1].split(".")[0]
+
+# funçao que retorna uma lista contendo o conteudo do arquivo .csv
+def get_list_values_from_table(csv_file):
+    csv_raw_rows = []
+    try:
+        with open(csv_file, "r", encoding="utf-8") as fp:
+            r = csv.reader(fp)
+            for i in r:
+                csv_raw_rows.append(i)
+            return csv_raw_rows
+    except FileNotFoundError:
+        print(f"Error: {csv_file} not found")
+        return []
+
+# funçao para retornar uma lista de dicionarios contendo o conteudo do arquivo .csv
+def get_csv_data(csv_file):
+    data = []
+    with open(csv_file, "r", encoding="utf-8") as fp:
+        file = csv.DictReader(fp)
+        for i in file:
+            data.append(dict(i))
+        return data
+
+# funçao para retornar apenas a segunda linha da tabela
+def get_first_value_from_table(csv_data):
+    return list(csv_data[1].values())
+
+def get_inferred_types(val):
+    inferred_types = []
+    for i in val:
+        t = get_types(i)
+        inferred_types.append(t)
+    return inferred_types
+
+def create_table(db, table_name, inferred_types, header):
+    sql_types = [sqlite_types(v) for v in inferred_types]
+
+    columns = ", ".join([f"{col} {sqltype}" for col, sqltype in zip(header, sql_types)])
+
+    sql = f"CREATE TABLE {table_name} ({columns})"
+
+    db.execute(sql)
+
+def insert_into_table(db, table_name, inferred_types, csv_raw_rows):
+    placeholders = ", ".join(["?"] * len(inferred_types))
+    params = [[get_types(val) for val in linha] for linha in csv_raw_rows[1:]]
+
+    sql = f"INSERT INTO {table_name} VALUES ({placeholders})"
+
+    db.executemany(sql, params)
+
+def confirm_commits(db):
+    confirm = input("Confirm changes (Y/N) =>")
+
+    if confirm.lower() == "y":
+        db.commit()
+    else:
+        print("Returning to main! Commits weren't completed")
+
+
+def main():
+    csv_file = input("Path to csv file =>")
+
+    db_file = input("Path to db file =>")
+
+    # alocando os dados do arquivo .csv em um dict
+    csv_dict_rows = get_csv_data(csv_file)
+
+    # alocando os dados do arquivo .csv em um list
+    csv_raw_rows = get_list_values_from_table(csv_file)
+
+    # pegando o cabeçalho da tabela
+    header = list(csv_dict_rows[0].keys())
+
+    #pegando valores da primeira linha para identificaçao
+    first_row_values = get_first_value_from_table(csv_dict_rows)
+
+    # pegando a linha da tabela e convertendo para os valores reais
+    # se for (string 22) ele vira apenas (inteiro 22)
+    inferred_types = get_inferred_types(first_row_values)
+
+    # pegando nome da tabela
+    table_name = get_table_name(csv_file)
+
+    db = Database(db_file)
+
+    create_table(db, table_name, inferred_types, header)
+
+    insert_into_table(db, table_name, inferred_types, csv_raw_rows)
+
+    confirm_commits(db)
+
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
